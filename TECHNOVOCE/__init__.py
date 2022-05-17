421177e930ee778aee14995778fdba4d83aa8067
Jim McGrath <james.mcgrath@manchester.ac.uk>
2022-05-17 20:30:50 +0100

    set up site structure; improve gitignore

--- TECHNOVOCE/__init__.py
+++ TECHNOVOCE/__init__.py
@@ -11,4 +11,16 @@ def create_app(test_config=None):
     def index():
         return render_template("index.html")
 
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
+
+    @app.route("/services")
+    def services():
+        return render_template("services.html")
+
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
+
     return app
