1953c1c92b23f4ba1a06afd2e86787c4f5a04c84
Jim McGrath <jim@technovoce.co.uk>
2023-06-25 14:46:33 +0100

    update build location

--- build.py
+++ build.py
@@ -1,7 +1,10 @@
 from flask_frozen import Freezer
 from TECHNOVOCE import create_app
 
-freezer = Freezer(create_app())
-
 if __name__ == "__main__":
+    app = create_app()
+    app.config['FREEZER_DESTINATION'] = "../build/"
+
+    freezer = Freezer(app)
+
     freezer.freeze()
